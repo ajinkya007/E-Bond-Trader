f5ced3fd783a9ddd4406aa0136e8f1935ec26971
keyalsaurav <keyalsaurav@gmai.com>
2016-08-29 15:04:41 +0530

    database sql

--- EbondShark/EbondSharkEJB/ejbModule/ebondshark/ejb/EbondSharkBean.java
+++ EbondShark/EbondSharkEJB/ejbModule/ebondshark/ejb/EbondSharkBean.java
@@ -8,7 +8,7 @@ import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+//import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import ebondshark.jpa.Bond;
 
